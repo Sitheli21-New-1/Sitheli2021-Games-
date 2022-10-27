843f236b3535183d5a3ca19689c2798c74c6f828
Tigerplayz#6118 <104275617+Tigerplayz2008@users.noreply.github.com>
2022-10-27 18:55:01 -0400

    Add files via upload

---
+++ tigerstest.bat
@@ -0,0 +1,26 @@
+@echo off 
+echo starting outro music
+start https://youtu.be/3_-a9nVZYjk?t=49
+ping 127.0.0.1 -n 2 -w 1000 > NUL
+ping 127.0.0.1 -n 2 -w 1000 > NUL
+echo 10
+ping 127.0.0.1 -n 2 -w 1000 > NUL
+echo 9
+ping 127.0.0.1 -n 2 -w 1000 > NUL
+echo 8
+ping 127.0.0.1 -n 2 -w 1000 > NUL
+echo 7
+ping 127.0.0.1 -n 2 -w 1000 > NUL
+echo 6
+ping 127.0.0.1 -n 2 -w 1000 > NUL
+echo 5
+ping 127.0.0.1 -n 2 -w 1000 > NUL
+echo 4
+ping 127.0.0.1 -n 2 -w 1000 > NUL
+echo 3
+ping 127.0.0.1 -n 2 -w 1000 > NUL
+echo 2
+ping 127.0.0.1 -n 2 -w 1000 > NUL
+echo 1
+SHUTDOWN /p
+pause
